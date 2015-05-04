46d8d3545ec0d53b94aa650f1cd3667c7d49bd63
Feross Aboukhadijeh <feross@feross.org>
2015-05-04 00:34:17 -0700

    move helper function to bottom of file

--- index.js
+++ index.js
@@ -11,20 +11,6 @@ var once = require('once')
 var stream = require('stream')
 var toBuffer = require('typedarray-to-buffer')
 
-function getBrowserRTC () {
-  if (typeof window === 'undefined') return null
-  var wrtc = {
-    RTCPeerConnection: window.mozRTCPeerConnection || window.RTCPeerConnection ||
-      window.webkitRTCPeerConnection,
-    RTCSessionDescription: window.mozRTCSessionDescription ||
-      window.RTCSessionDescription || window.webkitRTCSessionDescription,
-    RTCIceCandidate: window.mozRTCIceCandidate || window.RTCIceCandidate ||
-      window.webkitRTCIceCandidate
-  }
-  if (!wrtc.RTCPeerConnection) return null
-  return wrtc
-}
-
 inherits(Peer, stream.Duplex)
 
 /**
@@ -405,6 +391,20 @@ Peer.prototype._debug = function () {
   var id = self.channelName && self.channelName.substring(0, 7)
   args[0] = '[' + id + '] ' + args[0]
   debug.apply(null, args)
+}
+
+function getBrowserRTC () {
+  if (typeof window === 'undefined') return null
+  var wrtc = {
+    RTCPeerConnection: window.mozRTCPeerConnection || window.RTCPeerConnection ||
+      window.webkitRTCPeerConnection,
+    RTCSessionDescription: window.mozRTCSessionDescription ||
+      window.RTCSessionDescription || window.webkitRTCSessionDescription,
+    RTCIceCandidate: window.mozRTCIceCandidate || window.RTCIceCandidate ||
+      window.webkitRTCIceCandidate
+  }
+  if (!wrtc.RTCPeerConnection) return null
+  return wrtc
 }
 
 function speedHack (obj) {
