83a51013f4eb28e8f6d5f1c3778144a4be2a2885
Feross Aboukhadijeh <feross@feross.org>
2015-06-12 13:45:31 -0700

    use get-browser-rtc

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = Peer
 
 var debug = require('debug')('simple-peer')
+var getBrowserRTC = require('get-browser-rtc')
 var hat = require('hat')
 var inherits = require('inherits')
 var isTypedArray = require('is-typedarray')
@@ -501,20 +502,6 @@ Peer.prototype._debug = function () {
   var id = self.channelName && self.channelName.substring(0, 7)
   args[0] = '[' + id + '] ' + args[0]
   debug.apply(null, args)
-}
-
-function getBrowserRTC () {
-  if (typeof window === 'undefined') return null
-  var wrtc = {
-    RTCPeerConnection: window.mozRTCPeerConnection || window.RTCPeerConnection ||
-      window.webkitRTCPeerConnection,
-    RTCSessionDescription: window.mozRTCSessionDescription ||
-      window.RTCSessionDescription || window.webkitRTCSessionDescription,
-    RTCIceCandidate: window.mozRTCIceCandidate || window.RTCIceCandidate ||
-      window.webkitRTCIceCandidate
-  }
-  if (!wrtc.RTCPeerConnection) return null
-  return wrtc
 }
 
 function noop () {}
