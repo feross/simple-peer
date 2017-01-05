a352d961db752f0b2b31f566a2003f313335a682
Feross Aboukhadijeh <feross@feross.org>
2017-01-05 01:17:17 -0800

    test: fix rtc config url

--- test/common.js
+++ test/common.js
@@ -4,7 +4,7 @@ var thunky = require('thunky')
 exports.getConfig = thunky(function (cb) {
   // Includes TURN -- needed for tests to pass on Sauce Labs
   // https://github.com/feross/simple-peer/issues/41
-  get.concat('https://instant.io/rtcConfig', function (err, res, data) {
+  get.concat('https://instant.io/_rtcConfig', function (err, res, data) {
     if (err) return cb(err)
     data = data.toString()
     try {
