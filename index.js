2ef6bf0ba3e5594d2990656035b7d46e3b08bbaa
Feross Aboukhadijeh <feross@feross.org>
2017-01-10 21:04:20 -0800

    remove default deprecated iceServers[0].url property

--- index.js
+++ index.js
@@ -154,7 +154,6 @@ Peer.WEBRTC_SUPPORT = !!getBrowserRTC()
 Peer.config = {
   iceServers: [
     {
-      url: 'stun:23.21.150.121', // deprecated, replaced by `urls`
       urls: 'stun:23.21.150.121'
     }
   ]
