c469a113f48945e94fbf97ea36c89e281fef4a5f
Max Baluev <maxbaluev@outlook.com>
2017-04-13 15:50:27 +0500

    Fix channelName assign bug
    An incorrect name was assigned if the instance was not an initiator.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function Peer (opts) {
 
   stream.Duplex.call(self, opts)
 
-  opts.channelName ? self.channelName = opts.channelName : randombytes(20).toString('hex')
+  opts.channelName ? self.channelName = opts.channelName : self.channelName = randombytes(20).toString('hex')
 
   // Needed by _transformConstraints, so set this early
   self._isChromium = typeof window !== 'undefined' && !!window.webkitRTCPeerConnection
