1ff3a3e6991e34d715d121c686c55b3a2be0a944
RationalCoding <thomasmullendesign@gmail.com>
2018-04-12 20:20:19 -0400

    fix standard

--- test/z-cleanup.js
+++ test/z-cleanup.js
@@ -1,7 +1,6 @@
 // This test file runs after all the others. This is where we can run the cleanup
 // code that is required for the electron-webrtc
 
-var common = require('./common')
 var test = require('tape')
 
 test('cleanup', function (t) {
