44a968f8fd87ec3f4c67820a3aca8be0faba8e35
RationalCoding <thomasmullendesign@gmail.com>
2019-02-28 15:12:47 -0500

    invert condition

--- index.js
+++ index.js
@@ -761,7 +761,7 @@ Peer.prototype._maybeReady = function () {
           self.localAddress = local[0]
           self.localPort = Number(local[1])
         }
-        self.localFamily = self.localAddress.includes(':') ? 'IPv4' : 'IPv6'
+        self.localFamily = self.localAddress.includes(':') ? 'IPv6' : 'IPv4'
 
         var remote = remoteCandidates[selectedCandidatePair.remoteCandidateId]
 
@@ -779,7 +779,7 @@ Peer.prototype._maybeReady = function () {
           self.remoteAddress = remote[0]
           self.remotePort = Number(remote[1])
         }
-        self.remoteFamily = self.remoteAddress.includes(':') ? 'IPv4' : 'IPv6'
+        self.remoteFamily = self.remoteAddress.includes(':') ? 'IPv6' : 'IPv4'
 
         self._debug(
           'connect local: %s:%s remote: %s:%s',
