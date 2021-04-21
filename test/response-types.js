5ed69185befe90ab747f7d99d95f97434370a567
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-04-20 23:07:43 -0400

    Add response-types tests to both peers and add tests for replaceTrack stream

--- test/response-types.js
+++ test/response-types.js
@@ -4,6 +4,7 @@ const Peer = require("../");
 const test = require("tape");
 
 let config;
+
 test("get config", function (t) {
   common.getConfig(function (err, _config) {
     if (err) return t.fail(err);
@@ -12,75 +13,92 @@ test("get config", function (t) {
   });
 });
 
-test("adds sender and replace promise", function (t) {
-  t.plan(5);
+test("tests various request / response types in the API calls", function (t) {
+  t.plan(12);
 
   const peer1 = new Peer({
     config,
     initiator: true,
     wrtc: common.wrtc,
   });
+
   const peer2 = new Peer({
     config,
     wrtc: common.wrtc,
   });
 
+  // TODO: Include a way to run these as a single function definition
+  // (currently not able to because of other peer signaling)
   peer1.on("signal", function (data) {
-    if (data.renegotiate) t.fail("got unexpected request to renegotiate");
-    if (!peer2.destroyed) peer2.signal(data);
+    peer2.signal(data);
   });
+
   peer2.on("signal", function (data) {
-    if (data.renegotiate) t.fail("got unexpected request to renegotiate");
-    if (!peer1.destroyed) peer1.signal(data);
+    peer1.signal(data);
   });
 
-  peer1.on("connect", function () {
-    const stream = getMediaStream();
-    const track = stream.getTracks()[0];
+  // Run the same tests on both peers
+  [peer1, peer2].forEach((peer) => {
+    peer.on("connect", () => {
+      const stream = getMediaStream();
+      const track1 = stream.getTracks()[0];
+      const track2 = stream.getTracks()[1];
 
-    // Test with stream object
-    const sender = peer1.addTrack(track, stream);
+      const sender = peer.addTrack(track1, stream);
 
-    t.equal(
-      Object.prototype.toString.call(sender),
-      "[object RTCRtpSender]",
-      "sender is of RTCRtpSender type"
-    );
+      t.equal(
+        Object.prototype.toString.call(sender),
+        "[object RTCRtpSender]",
+        "sender is of RTCRtpSender type"
+      );
 
-    const track2 = track.clone();
+      // Missing stream should throw
+      t.throws(
+        () => {
+          peer.addTrack(track2);
+        },
+        {
+          message: "Stream is a required parameter",
+        },
+        "addTrack throws without stream parameter"
+      );
 
-    // Test without required parameter
-    t.throws(
-      () => {
-        peer1.addTrack(track2);
-      },
-      {
-        message: "Stream is a required parameter",
-      }
-    );
+      // Missing stream should throw
+      peer
+        .replaceTrack(track1, track2)
+        .then(() => {
+          // Not expected
+          t.fail("replaceTrack should not succeed without a stream");
+        })
+        .catch((err) => {
+          // Expected
+          t.ok(err, "replaceTrack throws without stream paramter");
+        });
 
-    const replacePromise = peer1.replaceTrack(track, track2, stream);
+      const replacePromise = peer.replaceTrack(track1, track2, stream);
 
-    t.equal(
-      Object.prototype.toString.call(replacePromise),
-      "[object Promise]",
-      "replacePromise is of Promise type"
-    );
+      t.equal(
+        Object.prototype.toString.call(replacePromise),
+        "[object Promise]",
+        "replacePromise is of Promise type"
+      );
 
-    // Test without required parameter
-    t.throws(
-      () => {
-        peer1.removeTrack(track2);
-      },
-      {
-        message: "Stream is a required parameter",
-      }
-    );
+      // Test without required parameter
+      t.throws(
+        () => {
+          peer.removeTrack(track2);
+        },
+        {
+          message: "Stream is a required parameter",
+        },
+        "removeTrack throws without stream parameter"
+      );
 
-    t.equal(
-      peer1.removeTrack(track, stream),
-      undefined,
-      "removeTrack returns nothing"
-    );
+      t.equal(
+        peer.removeTrack(track2, stream),
+        undefined,
+        "removeTrack returns nothing"
+      );
+    });
   });
 });
